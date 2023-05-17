b96ffe55e35b88651dd5918758c44343927817a6
Ahmet Eren Şakar <82838085+AhmetErenSakar@users.noreply.github.com>
2023-05-17 23:10:21 +0300

    Add files via upload

---
+++ Untitled-1.sql
@@ -0,0 +1,19 @@
+
+--SELECT title, description FROM film;
+--title, description verilerini sıralamak için gereken kod blogu
+
+--SELECT * FROM film 
+--WHERE length > 60 OR length <75;
+--length i 60 dan büyük olanlar ile 75 ten küçük olanların getiren kod blogu
+
+--SELECT * FROM film
+--WHERE rental_rate = 0.99 AND replacement_cost = 12.99 OR replacement_cost = 28.99;
+--rental_rate 0.99 ve replacement_cost = 12.99 veya 28.99 olan verileri getiren kod blogu
+
+--SELECT * FROM customer
+--WHERE first_name = 'Mary';
+--Cevap Smith
+
+--SELECT * FROM film
+--WHERE NOT length > 50 AND NOT rental_rate = 2.99 AND NOT rental_rate = 4.99;
+--CEVAP YUKARIDADIR
